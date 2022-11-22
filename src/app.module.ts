504c1eb5f8fd260eaa72a0e933c8226268889d4b
João Vitor Antunes Ferreira <projetos.joao236@gmail.com>
2022-11-21 22:23:35 -0300

    feat: import product module on asset module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AssetModule } from './asset/asset.module';
+import ProductModule from './product/product.module';
 
 @Module({
-  imports: [AssetModule],
+  imports: [AssetModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
