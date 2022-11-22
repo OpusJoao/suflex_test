07a2c4a764f22dcc696d350ea0be61cf73e7c033
João Vitor Antunes Ferreira <projetos.joao236@gmail.com>
2022-11-21 22:51:09 -0300

    feat: create the filters to get the products route

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Query } from "@nestjs/common";
 import ProductService from "./product.service";
 
 @Controller('product')
@@ -6,8 +6,21 @@ export default class ProductController{
     constructor(private readonly productService: ProductService){}
 
     @Get()
-    async findAll(){
-        return await this.productService.find({});
+    async findAll(@Query() query){
+        const { sort, daysToExpire } = query;
+        let sortLower = sort.toLowerCase();
+        let whereQuery = daysToExpire ? {
+            daysToExpiration: {
+                equals: Number(daysToExpire)
+            }
+        } : {}
+        if(sortLower !== 'asc' && sortLower !== 'desc') sortLower = 'asc'
+
+        return await this.productService.find({
+            where: whereQuery, 
+            orderBy: { name: sortLower }
+        } );
     }
+
 
 }
