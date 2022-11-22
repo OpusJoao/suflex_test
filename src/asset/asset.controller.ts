18243674f0476295239cfd4cfd55bff44e53773d
João Vitor Antunes Ferreira <projetos.joao236@gmail.com>
2022-11-21 22:22:21 -0300

    feat: create the methods to upload the asset to database

--- src/asset/asset.controller.ts
+++ src/asset/asset.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { parse } from 'papaparse';
+import ProductService from 'src/product/product.service';
 
 @Controller('asset')
-export class AssetController {}
+export class AssetController {
+    constructor(private readonly productService: ProductService){}
+    @Post('upload')
+    @UseInterceptors(FileInterceptor('file'))
+    async uploadFileToDatabase(@UploadedFile() file: Express.Multer.File) {
+        const csvData = file.buffer.toString()
+        
+        const parsedCsv = parse(csvData, {
+            header: true,
+            skipEmptyLines: true,
+            transformHeader: header => header.toLowerCase().trim(),
+            step: async (result) => { return await this.createItemInDatabase(result.data)},
+            complete: result => result.data,
+        })
+
+        return parsedCsv;
+    }
+
+    private async createItemInDatabase(data){
+        const { name, dias_para_vencimento } = data;
+        return this.productService.create({name, daysToExpiration: Number(dias_para_vencimento)})
+    }
+}
