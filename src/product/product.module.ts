bc6637f4853e852f5a30741f281ff5c096b49682
João Vitor Antunes Ferreira <projetos.joao236@gmail.com>
2022-11-21 21:50:23 -0300

    fix: insert prismaService as provider of ProductModule

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
 import ProductController from "./product.controller";
 import ProductService from "./product.service";
 
 @Module({
     controllers: [ProductController],
-    providers: [ProductService]
+    providers: [ProductService, PrismaService]
 })
 export default class ProductModule{}
