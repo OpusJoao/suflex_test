5535947eddd4a880991fb4165bd1e5a786618c8c
João Vitor Antunes Ferreira <projetos.joao236@gmail.com>
2022-11-21 21:45:43 -0300

    feat: create product controller and module

---
+++ src/product/product.controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from "@nestjs/common";
+import ProductService from "./product.service";
+
+@Controller('product')
+export default class ProductController{
+    constructor(private readonly productService: ProductService){}
+}
