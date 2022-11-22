bca0080ba6cde41976c5a1d0481788cd5c00733c
João Vitor Antunes Ferreira <projetos.joao236@gmail.com>
2022-11-21 22:23:13 -0300

    feat: import providers and modules on asset module

--- src/asset/asset.module.ts
+++ src/asset/asset.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import ProductService from 'src/product/product.service';
 import { AssetController } from './asset.controller';
 
 @Module({
-  controllers: [AssetController]
+  controllers: [AssetController],
+  providers: [ProductService, PrismaService]
 })
 export class AssetModule {}
