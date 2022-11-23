499d2860315bf3aea843b687ba9957c137cdae63
João Vitor Antunes Ferreira <projetos.joao236@gmail.com>
2022-11-23 19:49:20 -0300

    refactor: remove unecessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AssetModule } from './asset/asset.module';
 import ProductModule from './product/product.module';
 
 @Module({
   imports: [AssetModule, ProductModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
